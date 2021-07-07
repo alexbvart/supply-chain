b3b0263b64af54603e78212a5e781c7b15e934bb
Ismael Grimaldo <ismaelgrive@gmail.com>
2021-07-06 23:58:55 -0500

    validating special inputs

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import EnterpriseInfo from '../src/container/EnterpriseInfo/EnterpriseInfo'
+import InputForm from '../src/components/InputForm/InputForm'
 
 import Head from 'next/head'
 import DetailSideBar from '../src/container/DetailSideBar'
@@ -28,7 +29,25 @@ export default function Home() {
 
   return (
     <>
-      <DetailSideBar title="Clientes" data={data}></DetailSideBar>
+      {/* <DetailSideBar title="Clientes" data={data}></DetailSideBar> */}
+      <form action="aja.php" id="info_form">
+        <InputForm 
+          type={'text'}
+          name={'name'} 
+          placeholder={'Razón social'} 
+          maxLength={80}
+          pattern={'[a-z]'}
+          title={'Debe contener solamente letras'}
+        />
+        <InputForm 
+          type={'tel'}
+          name={'phone'} 
+          placeholder={'Teléfono'} 
+          maxLength={10}
+          pattern={'[0-9]'} 
+          title={'Debe contener únicamente números del 0 al 9'}
+        />
+      </form>
     </>
   )
 }
--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { input_container, characters_remaining, input } from './inputForm.module.css'
 
 const InputForm = ({ type, name, placeholder, maxLength, pattern, title}) => {
-  function lengthLevels({element, maxLength, currentLength}){
+  function lengthWarns({element, maxLength, currentLength}){
     const secondWarning = Math.ceil(maxLength * .66)
     const firstWarning = Math.ceil(maxLength * .33)
 
@@ -12,15 +12,25 @@ const InputForm = ({ type, name, placeholder, maxLength, pattern, title}) => {
       return
     }
     if(currentLength >= firstWarning && currentLength < secondWarning){
-      element.style.background = 'gold'
+      element.style.background = 'rgba(251, 255, 0, 0.746)'
       element.style.color = 'black' 
       return
     }
-
+    
     if(currentLength >= secondWarning){
-      element.style.background = 'red'
+      element.style.background = 'rgba(255, 0, 0, 0.657)'
       element.style.color = 'white' 
       return
+    }
+  }
+
+  function validateSpecialInput({element, name, maxLength, alert}){
+    if(name === 'ruc' || name === 'phone'){
+          
+    }
+
+    if(name === 'dni'){
+
     }
   }
   return(
@@ -38,7 +48,11 @@ const InputForm = ({ type, name, placeholder, maxLength, pattern, title}) => {
           const inputContainerElement = event.target.parentElement
           const charsRemainingElement = inputContainerElement.querySelector('#charsRemaining')
           charsRemainingElement.textContent = `${(maxLength - currentLength)}`
-            lengthLevels({
+
+          if(name === 'phone' || name === 'dni'){
+            return
+          }
+            lengthWarns({
               element: charsRemainingElement.parentElement,
               maxLength,
               currentLength
